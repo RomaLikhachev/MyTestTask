80dbdc3b2a45b7a6b6646c4c904d878dd27f5ec5
Roman Likhachev <likhachevroman1996@gmail.com>
2019-11-08 02:16:43 +0400

    Create a project. Git installation and file indexing.

---
+++ app/src/main/java/com/test/mytesttask/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.test.mytesttask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
