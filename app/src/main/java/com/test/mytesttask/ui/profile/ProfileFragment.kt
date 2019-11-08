754de6c849dc29f182d220964bee360a1db2f873
Roman Likhachev <likhachevroman1996@gmail.com>
2019-11-08 04:30:21 +0400

    Create "Profile" section UI: fragment and ViewModel.

--- app/src/main/java/com/test/mytesttask/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/test/mytesttask/ui/profile/ProfileFragment.kt
@@ -4,29 +4,54 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.navArgs
 import com.test.mytesttask.R
+import com.test.mytesttask.databinding.FragmentProfileBinding
 import dagger.android.support.DaggerFragment
+import javax.inject.Inject
 
 class ProfileFragment : DaggerFragment() {
 
-    companion object {
-        fun newInstance() = ProfileFragment()
-    }
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
 
     private lateinit var viewModel: ProfileViewModel
+
+    private val navArgs: ProfileFragmentArgs by navArgs()
+
+    private lateinit var dataBinding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
+        return dataBinding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
+        viewModel = ViewModelProviders
+            .of(this, viewModelFactory)
+            .get(ProfileViewModel::class.java)
+
+        viewModel.setEmployeeId(navArgs.employeeId)
+
+        viewModel.employee.observe(viewLifecycleOwner, Observer { employee ->
+            employee?.let {
+                dataBinding.employee = employee
+            }
+        })
+
+        viewModel.specialtiesTitle.observe(viewLifecycleOwner, Observer {  specialtiesTitle ->
+            specialtiesTitle?.let {
+                dataBinding.specialtiesTitle = specialtiesTitle
+            }
+        })
+    }
 }
--- app/src/main/java/com/test/mytesttask/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/test/mytesttask/ui/profile/ProfileViewModel.kt
@@ -1,6 +1,11 @@
 package com.test.mytesttask.ui.profile
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.test.mytesttask.model.Employee
+import com.test.mytesttask.model.Specialty
 import com.test.mytesttask.repository.EmployeeRepository
 import javax.inject.Inject
 
@@ -8,4 +13,30 @@ class ProfileViewModel @Inject constructor(
     val repository: EmployeeRepository
 ) : ViewModel() {
 
+    private val _employeeId = MutableLiveData<Int>()
+    val employeeId : LiveData<Int>
+        get() = _employeeId
+
+    val employee : LiveData<Employee> = Transformations
+        .switchMap(_employeeId) { _employeeId ->
+            repository.getEmployee(_employeeId)
+        }
+
+    private val specialtiesList : LiveData<List<Specialty>> = Transformations
+        .switchMap(employee) { employee ->
+            repository.getSpecialtiesForEmployee(employee.id)
+        }
+
+    val specialtiesTitle: LiveData<String> = Transformations
+        .map(specialtiesList) { specialtiesList ->
+            specialtiesList.joinToString(separator = ", ") {
+                    specialty -> specialty.name
+            }
+        }
+
+    internal fun setEmployeeId(employeeId: Int) {
+        if (_employeeId.value != employeeId) {
+            _employeeId.value = employeeId
+        }
+    }
 }
